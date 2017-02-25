a5e3dd431c5ba393a773f399682990a2b5578a2f
Sinamindev <sinamindev@gmail.com>
2017-02-24 19:49:53 -0800

    responding to user input in text field

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -8,22 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var diceBoxNameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Handle the text field's user input through delegate callbacks
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        diceBoxNameLabel.text = textField.text
+    }
+    
     //MARK: Actions
-
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         diceBoxNameLabel.text = "Default text"
     }
