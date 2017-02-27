116bcb53dd254122872c8045fd98229a4f4d103a
Sina <sinamindev@gmail.com>
2017-02-26 22:45:26 -0800

    Expanded the buttons into a resizable array for dice representation

--- Dice/RatingControl.swift
+++ Dice/RatingControl.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
+    
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0
+    
     //MARK: Initialization
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupButtons()
@@ -20,21 +27,35 @@ class RatingControl: UIStackView {
         setupButtons()
     }
     
+    //MARK: Button Action
+    
+    func ratingButtonTapped(button: UIButton) {
+        print("Button pressed")
+    }
+    
     //MARK: Private Methods
     
-    private func setupButtons(){
-        // Create button
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
+    private func setupButtons() {
         
-        // Add Constraints
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
-        
-        // Add the button to the stack
-        addArrangedSubview(button)
-        
+        for _ in 0..<5 {
+            // Create button
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            
+            // Add Constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            // Add the button to the stack
+            addArrangedSubview(button)
+            
+            // Add the new button to the rating button array
+            ratingButtons.append(button)
+        }
     }
 
 
