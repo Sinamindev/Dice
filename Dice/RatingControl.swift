b385878cea67bb0bb30d9a995c1a33a1bccb5a34
Sina <sinamindev@gmail.com>
2017-02-26 22:04:02 -0800

    Added button

---
+++ Dice/RatingControl.swift
@@ -0,0 +1,42 @@
+//
+//  RatingControl.swift
+//  Dice
+//
+//  Created by Sina on 2/26/17.
+//  Copyright © 2017 Sina. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Private Methods
+    
+    private func setupButtons(){
+        // Create button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add Constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
+        
+    }
+
+
+
+}
