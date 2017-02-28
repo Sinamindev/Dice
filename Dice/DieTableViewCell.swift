1615539d1754236f00cd8f9a5d31b67cf9596135
Sina <sinamindev@gmail.com>
2017-02-28 01:20:23 -0800

    Added a table view for selecting dice boxes

---
+++ Dice/DieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DieTableViewCell.swift
+//  Dice
+//
+//  Created by Sina on 2/28/17.
+//  Copyright © 2017 Sina. All rights reserved.
+//
+
+import UIKit
+
+class DieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
