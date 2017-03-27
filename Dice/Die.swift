093148ca9ebf388f306ceefbe12fc80762a7e015
Sina <sinamindev@gmail.com>
2017-03-26 17:31:56 -0700

    Setting up persistant data

--- Dice/Die.swift
+++ Dice/Die.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
+import os.log
 
 
-class Die {
+class Die: NSObject, NSCoding {
     
     //MARK: Properties
+    
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
     
     var name: String
     var photo: UIImage?
@@ -38,6 +45,30 @@ class Die {
         self.rating = rating
         
         
+    }
+    
+    //MARK: NSCoding
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        // The name is required. If we cannot decode a name string, the initializer should fail.
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Die object.", log: OSLog.default, type: .debug)
+            return nil
+            
+            // Because photo is an optional property of Die, just use conditional cast.
+            let photo = aDecoder.decodeObjectForKey(PropertyKey.photo) as? UIImage
+            
+            let rating = aDecoder.decodeIntegerForKey(PropertyKey.rating)
+            
+            // Must call designated initializer.
+            self.init(name: name, photo: photo, rating: rating)
+        }
     }
 
 }
